5d1e791e5aa2682494734e5a1769b3be423b01f8
anh huynh <huynhanh8327@gmail.com>
2024-05-09 13:47:40 -0700

    reverse link order

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,18 @@
-/home/ahuynh/.config/fish/config.fish
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+fish_add_path --path $PATH
+
+export EDITOR=vim
+export VISUAL=vim
+
+set fish_greeting
+fastfetch
+starship init fish | source
+
+#common aliases
+alias quit "exit"
+alias vim "sudo -E vim"
+alias gitlog "git log -5 --stat"
+alias top bpytop
